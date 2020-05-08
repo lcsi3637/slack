7911403e3e622fd49c1a8209be3355c189e59582
saban inci <64227008+sabaninci@users.noreply.github.com>
2020-05-08 15:47:24 -0400

    Update LogCall.java
    i fixed my bugs

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -1,5 +1,7 @@
 package day_1;
 
 public class LogCall {
+  public static void main(String[] arg){
   System.out.println(" i added this phrase form github than i m doing pulling from here to IntelliJ");
+}
 }
